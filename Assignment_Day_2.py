2187e6624f080716e6a6e696bfd8a6fc249d2d7a
sreeakhila07 <68080940+sreeakhila07@users.noreply.github.com>
2020-12-10 01:10:11 +0530

    Akhilasree_M_Assignment_1_Day_2

---
+++ Assignment_Day_2.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1
+# 
+
+# Try 5 Different functions of the String in Python.
+# 
+
+# I have used count,isalpha,find,islower,swapcase
+
+# In[9]:
+
+
+a="Akhilasree"
+
+
+# In[13]:
+
+
+a.count('e')
+
+
+# In[14]:
+
+
+a.isalpha()
+
+
+# In[15]:
+
+
+a.find("l")
+
+
+# In[16]:
+
+
+a.islower()
+
+
+# In[17]:
+
+
+b=" Madesh"
+
+
+# In[19]:
+
+
+a.swapcase()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# Question 2
+
+# Try 5 Different functions of the List object in Python
+
+# I have used append,pop,extend,count,sort
+
+# In[21]:
+
+
+l=[1,2,3,4,5]
+
+
+# In[22]:
+
+
+li=[6,7,8,9,10]
+
+
+# In[23]:
+
+
+l.append(8)
+
+
+# In[24]:
+
+
+l
+
+
+# In[26]:
+
+
+l.pop(3)
+
+
+# In[27]:
+
+
+l.extend(li)
+
+
+# In[28]:
+
+
+l
+
+
+# In[29]:
+
+
+l.count(8)
+
+
+# In[30]:
+
+
+l.sort()
+
+
+# In[31]:
+
+
+l
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# Question 3
+
+# Experiment with at least 5 default functions of Dictionary
+
+# I have used get,keys,items,values,popitem
+
+# In[32]:
+
+
+d={1:'a',2:'b',3:'c'}
+
+
+# In[36]:
+
+
+d.get(1)
+
+
+# In[38]:
+
+
+d.keys()
+
+
+# In[39]:
+
+
+d.items()
+
+
+# In[40]:
+
+
+d.values()
+
+
+# In[42]:
+
+
+d.popitem()
+
+
+# In[ ]:
+
+
+
+
